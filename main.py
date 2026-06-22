cd06dcde6e9b0f193c27101c9bda920155092d6f
Agathe OLIVIER <olivieragathe59@gmail.com>
2026-06-22 18:02:55 +0200

    Débug Page Accueil

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify, render_template
 from app.services.accueil_service import Affiche_Animal
 
-main= Flask(__name__)
+main = Flask(__name__, template_folder='app/templates')
 
 @main.route('/')
-def recup_animaux():
-    return jsonify(Affiche_Animal())
-
 def AffichePage():
     return render_template("accueil.html")
+
+@main.route('/Recupanimaux')
+def recup_animaux():
+    return jsonify(Affiche_Animal())
 
 if __name__ == '__main__':
     main.run(debug=True)
